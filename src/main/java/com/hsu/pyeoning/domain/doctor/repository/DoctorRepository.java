b2b587092dc6a20bb39f5dceac48d2eeddfda1b4
jungeun-e <lucycy255@gmail.com>
2024-10-28 00:39:51 +0900

    ✨ DoctorRepository 생성

---
+++ src/main/java/com/hsu/pyeoning/domain/doctor/repository/DoctorRepository.java
@@ -0,0 +1,9 @@
+package com.hsu.pyeoning.domain.doctor.repository;
+
+import com.hsu.pyeoning.domain.doctor.entity.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DoctorRepository  extends JpaRepository<Doctor, Long> {
+}
