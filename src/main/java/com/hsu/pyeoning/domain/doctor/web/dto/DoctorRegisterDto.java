1da79e475e88ba74ded25a2b6e22764f8a49639c
jungeun-e <lucycy255@gmail.com>
2024-10-30 13:42:07 +0900

    ✨ 의사 비밀번호 설정 조건 추가

--- src/main/java/com/hsu/pyeoning/domain/doctor/web/dto/DoctorRegisterDto.java
+++ src/main/java/com/hsu/pyeoning/domain/doctor/web/dto/DoctorRegisterDto.java
@@ -1,7 +1,7 @@
 package com.hsu.pyeoning.domain.doctor.web.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,5 +20,7 @@ public class DoctorRegisterDto {
     private Long doctorLicense;
 
     @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "비밀번호는 영문/숫자/특수기호를 조합하여 8자 이상 설정해야 합니다.")
     private String doctorPassword;
 }
