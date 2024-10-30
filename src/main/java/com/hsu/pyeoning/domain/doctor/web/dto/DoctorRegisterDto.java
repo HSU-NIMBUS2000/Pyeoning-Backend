2dd5681b8a44c4997a2dc7a1d4204a75d38b9482
jungeun-e <lucycy255@gmail.com>
2024-10-30 13:37:55 +0900

    🎨 DoctorRegisterDto 어노테이션 수정

--- src/main/java/com/hsu/pyeoning/domain/doctor/web/dto/DoctorRegisterDto.java
+++ src/main/java/com/hsu/pyeoning/domain/doctor/web/dto/DoctorRegisterDto.java
@@ -1,5 +1,6 @@
 package com.hsu.pyeoning.domain.doctor.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,15 +10,15 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class DoctorRegisterDto {
-    @NotNull(message = "이름을 입력해주세요.")
+    @NotBlank(message = "이름을 입력해주세요.")
     private String doctorName;
 
-    @NotNull(message = "소속병원을 입력해주세요.")
+    @NotBlank(message = "소속병원을 입력해주세요.")
     private String doctorHospital;
 
-    @NotNull(message = "면허번호를 입력해주세요.")
+    @NotBlank(message = "면허번호를 입력해주세요.")
     private Long doctorLicense;
 
-    @NotNull(message = "비밀번호를 입력해주세요.")
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String doctorPassword;
 }
