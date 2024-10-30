d4cabe8f42f7c73df7e65a58d05fdb807a45c97b
jungeun-e <lucycy255@gmail.com>
2024-10-30 14:55:00 +0900

    ✨ 의사 면허 중복 검사 기능 구현

--- src/main/java/com/hsu/pyeoning/domain/doctor/service/DoctorServiceImpl.java
+++ src/main/java/com/hsu/pyeoning/domain/doctor/service/DoctorServiceImpl.java
@@ -2,6 +2,7 @@ package com.hsu.pyeoning.domain.doctor.service;
 
 import com.hsu.pyeoning.domain.doctor.entity.Doctor;
 import com.hsu.pyeoning.domain.doctor.repository.DoctorRepository;
+import com.hsu.pyeoning.domain.doctor.web.dto.CheckLicenseDto;
 import com.hsu.pyeoning.domain.doctor.web.dto.DoctorRegisterDto;
 import com.hsu.pyeoning.global.response.CustomApiResponse;
 import lombok.RequiredArgsConstructor;
@@ -16,7 +17,15 @@ public class DoctorServiceImpl implements DoctorService {
     private final DoctorRepository doctorRepository;
     private final PasswordEncoder passwordEncoder;
 
+    // 의사 등록
     public ResponseEntity<CustomApiResponse<?>> registDoctor(DoctorRegisterDto dto) {
+
+        // 면허 번호 중복 확인
+        if (doctorRepository.findByDoctorLicense(dto.getDoctorLicense()).isPresent()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(CustomApiResponse.createFailWithout(409, "이미 등록된 면허 번호입니다."));
+        }
+
         Doctor doctor = Doctor.builder()
                 .doctorName(dto.getDoctorName())
                 .doctorHospital(dto.getDoctorHospital())
@@ -27,4 +36,19 @@ public class DoctorServiceImpl implements DoctorService {
 
         return ResponseEntity.ok(CustomApiResponse.createSuccess(200, null, "의사 등록에 성공했습니다."));
     }
+
+    // 의사 면허 중복 확인
+    public ResponseEntity<CustomApiResponse<?>> checkLicenseNumber(CheckLicenseDto dto) {
+        boolean isDuplicate = isLicenseNumberDuplicate(dto.getDoctorLicense());
+        if (isDuplicate) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(CustomApiResponse.createFailWithout(409, "이미 등록된 면허 번호입니다."));
+        }
+        return ResponseEntity.ok(CustomApiResponse.createSuccess(200, null, "사용 가능한 면허 번호입니다."));
+    }
+
+    public boolean isLicenseNumberDuplicate(Long licenseNumber) {
+        return doctorRepository.findByDoctorLicense(licenseNumber).isPresent();
+    }
+
 }
