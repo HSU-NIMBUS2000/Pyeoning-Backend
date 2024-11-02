51acc2a33acddf2696440cbca99fa4634d1d7fd3
jungeun-e <lucycy255@gmail.com>
2024-11-03 01:46:52 +0900

    ✨ Chat 엔티티 생성

---
+++ src/main/java/com/hsu/pyeoning/domain/chat/entity/Chat.java
@@ -0,0 +1,33 @@
+package com.hsu.pyeoning.domain.chat.entity;
+
+import com.hsu.pyeoning.domain.doctor.entity.Doctor;
+import com.hsu.pyeoning.domain.patient.entity.Patient;
+import com.hsu.pyeoning.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "CHAT")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Chat extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "chat_id")
+    private Long chatId; // 채팅 PK
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patient_id", nullable = false)
+    private Patient patient; // 환자 FK
+
+    @Column(name = "chat_is_send", nullable = false)
+    private boolean chatIsSend; // 수신 및 발신
+
+    @Lob
+    @Column(name = "chat_content")
+    private String chatContent; // 채팅 내용
+
+}
