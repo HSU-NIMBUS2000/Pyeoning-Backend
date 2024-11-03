7501bd17ba74dfa98ddb5b2b58be823de32df68d
jungeun-e <lucycy255@gmail.com>
2024-11-04 01:42:03 +0900

    ♻️ 대화 내용 조회 응답 Dto 분리

--- src/main/java/com/hsu/pyeoning/domain/chat/web/dto/ChatResponseDto.java
+++ src/main/java/com/hsu/pyeoning/domain/chat/web/dto/ChatResponseDto.java
@@ -1,5 +1,6 @@
 package com.hsu.pyeoning.domain.chat.web.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class ChatResponseDto {
     private Long chatId;
     private int chatIsSend;
