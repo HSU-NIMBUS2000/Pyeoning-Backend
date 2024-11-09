b6d26afe02b00d38d1b0657c989f3584afde70b6
khyaejin <olivia019182@naver.com>
2024-11-09 11:57:16 +0900

    🚨 FIX. jwtTokenFilter 수정

--- src/main/java/com/hsu/pyeoning/global/security/jwt/filter/JwtTokenFilter.java
+++ src/main/java/com/hsu/pyeoning/global/security/jwt/filter/JwtTokenFilter.java
@@ -1,5 +1,7 @@
 package com.hsu.pyeoning.global.security.jwt.filter;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hsu.pyeoning.global.security.jwt.JwtTokenProvider;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -7,9 +9,11 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.filter.GenericFilterBean;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @RequiredArgsConstructor
 public class JwtTokenFilter extends GenericFilterBean {
@@ -28,9 +32,27 @@ public class JwtTokenFilter extends GenericFilterBean {
             return;
         }
 
+        // 1. 헤더에서 토큰 확인
         String token = jwtTokenProvider.resolveToken(request);
+
+        // 2. 헤더에 토큰이 없으면 바디에서 토큰을 확인
+        if (token == null) {
+            String body = StreamUtils.copyToString(request.getInputStream(), StandardCharsets.UTF_8);
+
+            // "token": "Bearer "로 시작하는 토큰 값 추출
+            if (body.contains("\"token\": \"Bearer ")) {
+                int startIndex = body.indexOf("\"token\": \"Bearer ") + 10; // "token": " 이후 인덱스
+                int endIndex = body.indexOf("\"", startIndex);
+                if (endIndex > startIndex) {
+                    token = body.substring(startIndex, endIndex);
+                }
+            }
+        }
+
+        // log
         System.out.println("token: " + token);
 
+        // 3. 토큰 유효성 검증 & SecurityContext에 설정
         if (token != null && jwtTokenProvider.validateToken(token)) {
             jwtTokenProvider.setSecurityContext(token);
         }
